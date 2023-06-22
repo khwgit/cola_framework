21553e9966b340adc5fc02c432435d61df8f15d4
Wong Ka Hei <jhuangamust@gmail.com>
2023-06-22 15:23:38 +0800

    fix: fix function log

--- lib/src/services/app_service_base.dart
+++ lib/src/services/app_service_base.dart
@@ -3,7 +3,9 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logging/logging.dart';
+import 'package:stack_trace/stack_trace.dart';
 
+import '../../utils.dart';
 import 'app_state_base.dart';
 
 abstract class AppServiceBase<State extends AppStateBase>
@@ -17,46 +19,48 @@ abstract class AppServiceBase<State extends AppStateBase>
 
   @protected
   void setLoggingStyle() {
-    hierarchicalLoggingEnabled = true;
-    final logger = this.logger.parent ?? Logger.root;
-    if (kDebugMode) {
-      recordStackTraceAtLevel = Level.SEVERE;
-      logger.level = Level.ALL;
-    }
-
-    logger.onRecord.listen((record) {
-      // Refernce: https://en.wikipedia.org/wiki/ANSI_escape_code
-      const end = '\x1b[0m';
-      final start = switch (record.level.name) {
-        'INFO' => '\x1b[34m',
-        'WARNING' => '\x1b[33m',
-        'SEVERE' => '\x1b[103m\x1b[31m',
-        'SHOUT' => '\x1b[41m\x1b[93m',
-        _ => '\x1b[90m',
-      };
-
-      log(
-        '$start${record.message}$end',
-        time: record.time,
-        sequenceNumber: record.sequenceNumber,
-        level: record.level.value,
-        name: record.loggerName,
-        zone: record.zone,
-        error: record.error,
-        stackTrace: record.stackTrace,
-      );
-
-      if (record.level >= Level.SEVERE) {
-        final error = record.error;
-        FlutterError.dumpErrorToConsole(
-          FlutterErrorDetails(
-            exception: error is Exception ? error : Exception(error),
-            stack: record.stackTrace,
-            library: record.loggerName,
-            context: ErrorDescription(record.message),
-          ),
-        );
+    debug(() {
+      hierarchicalLoggingEnabled = true;
+      final logger = this.logger.parent ?? Logger.root;
+      if (kDebugMode) {
+        recordStackTraceAtLevel = Level.SEVERE;
+        logger.level = Level.ALL;
       }
+
+      logger.onRecord.listen((record) {
+        // Refernce: https://en.wikipedia.org/wiki/ANSI_escape_code
+        const end = '\x1b[0m';
+        final start = switch (record.level.name) {
+          'INFO' => '\x1b[34m',
+          'WARNING' => '\x1b[33m',
+          'SEVERE' => '\x1b[103m\x1b[31m',
+          'SHOUT' => '\x1b[41m\x1b[93m',
+          _ => '\x1b[90m',
+        };
+
+        log(
+          '$start${record.message}$end',
+          time: record.time,
+          sequenceNumber: record.sequenceNumber,
+          level: record.level.value,
+          name: record.loggerName,
+          zone: record.zone,
+          error: record.error,
+          stackTrace: record.stackTrace,
+        );
+
+        if (record.level >= Level.SEVERE) {
+          final error = record.error;
+          FlutterError.dumpErrorToConsole(
+            FlutterErrorDetails(
+              exception: error is Exception ? error : Exception(error),
+              stack: record.stackTrace,
+              library: record.loggerName,
+              context: ErrorDescription(record.message),
+            ),
+          );
+        }
+      });
     });
   }
 
@@ -64,13 +68,14 @@ abstract class AppServiceBase<State extends AppStateBase>
   ///
   /// It is used for debug purpose.
   @protected
-  void trace(
-    Function function, [
+  void trace([
     List<dynamic> arguments = const [],
   ]) {
-    logger.fine(
-      '[${function.toString().split('\'')[1].trim()}(${arguments.map((e) => e.toString()).join(',')})]',
-    );
+    debug(() {
+      logger.fine(
+        '[${Trace.current(1).frames[0].member}(${arguments.map((e) => e.toString()).join(',')})]',
+      );
+    });
   }
 
   @override
